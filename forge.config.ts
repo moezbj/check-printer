d255567132cacd0ed70f1688cc2742ccfabc4d45
moez <moezbj.93@gmail.com>
2025-03-12 00:21:35 +0100

    fix..

--- forge.config.ts
+++ forge.config.ts
@@ -10,7 +10,7 @@ import { FuseV1Options, FuseVersion } from "@electron/fuses";
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
-    extraResource:["public", "/src/assets"]
+    extraResource:["public", "./src/assets"]
   },
   rebuildConfig: {},
   makers: [
